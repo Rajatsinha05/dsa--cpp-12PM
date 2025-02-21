e9c4643db253ea15a07cc8e1834f4bdd2a3e8278
rajatsinha05 <rajatsinha5467@gmail.com>
2025-02-21 12:54:27 +0530

    git lecture

---
+++ lecture2/lecture.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // datatype
+    // int,string,char,bool,float,double
+    // int num = 10;
+    // string str = "cpp";
+    // char c = 'a';
+    // bool b = false;
+    // cout << num;
+    // int num;
+    // cout << "enter any number" << endl;
+    // cin >> num;
+    // cout << num << endl;
+    // opr +-*/ %
+    // int a;
+    // int b;
+    // cout << "enter any number for a" << endl;
+    // cin >> a;
+    // cout << "enter any number for b" << endl;
+    // cin >> b;
+
+    // // // int sub = a - b;
+    // // cout << "sum of a and b " << a + b << endl;
+    // cout << a % b << endl;
+
+    //  ++,--,== ,<,> , ! , and ,or  && ,||
+    //  pre, post
+
+    int a = 10;
+    int b = 10;
+    int c = 20;
+    int d = 20;
+    bool result = ;
+    cout << result << endl;
+
+    return 0;
+}
